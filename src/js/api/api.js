231c2a392af466b6676306bb8f296e135f5b466c
ink-0 <71919983+ink-0@users.noreply.github.com>
2021-07-24 05:14:18 +0900

    feat: POST User
    - api-/etUserLsit, postUser 생성
    - User Input UI 생성
    - User 추가시  POST요청 후 UserList 변경
    - ink-0/js-todo-list-step2/#4

--- src/js/api/api.js
+++ src/js/api/api.js
@@ -4,9 +4,9 @@ const API = {
   ITEM: '/items',
 };
 
-const request = async (url) => {
+const request = async (url, body) => {
   try {
-    const res = await fetch(url);
+    const res = await fetch(url, body);
     if (!res.ok) {
       throw new Error(error);
     }
@@ -15,10 +15,34 @@ const request = async (url) => {
     throw new Error(`오류입니다.${e.message}`);
   }
 };
+// const requestPost = async (url, data) => {
+//   const res = await fetch(url, {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+//   });
+// };
 
+const getUserAddBody = (data) => {
+  return {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  };
+};
 const getUserList = async () => {
   const userList = await request(END_POINT + API.USER);
   return userList;
 };
 
-export { getUserList };
+const postUser = async (userName) => {
+  const bodyTemplate = { name: userName };
+  const userAddBody = getUserAddBody(bodyTemplate);
+  const a = await request(END_POINT + API.USER, userAddBody);
+  return a;
+};
+export { getUserList, postUser };
--- src/js/component/todo/TodoUser.js
+++ src/js/component/todo/TodoUser.js
@@ -1,19 +1,40 @@
-import { getUserList } from '../../api/api.js';
+import { getUserList, postUser } from '../../api/api.js';
 
 export default function TodoUser({ $app, initialState }) {
   this.state = initialState;
   this.$todoUserTarget = document.createElement('section');
   this.$todoUserTarget.className = 'userapp';
+  this.isAddUser = true;
   $app.appendChild(this.$todoUserTarget);
 
   this.setState = (newState) => {
     this.state = newState;
-
     this.render();
   };
+
+  this.$todoUserTarget.addEventListener('click', (e) => {
+    const $node = e.target;
+    if ($node.classList[1] === 'user-create-button') {
+      // console.log($node);
+      this.isAddUser = !this.isAddUser;
+      this.render();
+    }
+  });
+
+  this.$todoUserTarget.addEventListener('keydown', async (e) => {
+    const $node = e.target;
+    if ($node.className === 'user-input') {
+      if (e.keyCode === 13) {
+        console.log($node.value);
+        const hi = await postUser($node.value);
+        console.log('*****', hi);
+        this.render();
+      }
+    }
+  });
+
   this.render = async () => {
     const userList = await getUserList();
-
     const todoUserTemplate = `<div id="user-list">${userList.map(
       (user) => `<button class="ripple" id=${user._id}>${user.name}</button>`
     )}
@@ -23,6 +44,11 @@ export default function TodoUser({ $app, initialState }) {
       <button class="ripple user-delete-button" data-action="deleteUser">
         삭제 -
       </button>
+      ${
+        this.isAddUser
+          ? `<input class='user-input' placeholder='최소 2글자'/>`
+          : ''
+      }
     </div>`;
     this.$todoUserTarget.innerHTML = todoUserTemplate;
   };
